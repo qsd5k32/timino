33c705743783da337566605d4bdab4f29f7c3d23
lotfio lakehal <contact@lotfio-lakehal.com>
2017-08-25 21:28:17 +0100

    db config file

---
+++ App/Config/Database.php
@@ -0,0 +1,44 @@
+<?php
+
+/** 
+ * Tinimo - PHP MVC framework
+ *
+ * @package     Tinimo
+ * @author      Lotfio Lakehal <contact@lotfio-lakehal.com>
+ * @copyright   2017 Lotfio Lakehal
+ * @license     MIT
+ * @link        https://github.com/lotfio-lakehal/timino
+ * 
+ * Copyright (C) 2018
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * INFO :
+ * Database Configuration file
+ * 
+ */
+
+return[
+
+   "DRIVER" => "mysql",
+   "HOST"   => "127.0.0.1",
+   "NAME"   => "Timino",
+   "USER"   => "root",
+   "PASS"   => "123456",
+
+   "OPTIONS" =>[
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+      PDO::ATTR_EMULATE_PREPARES   => false,
+      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+   ]
+   
+];
