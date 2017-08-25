effa0bed95547879e4da25339433966ae5ebb363
lotfio lakehal <contact@lotfio-lakehal.com>
2017-08-25 21:31:07 +0100

    db con service

---
+++ App/Services/Database/Connection.php
@@ -0,0 +1,73 @@
+<?php
+
+/** 
+ * Timino - PHP MVC framework
+ *
+ * @package     Timino
+ * @author      Lotfio Lakehal <contact@lotfio-lakehal.com>
+ * @copyright   2017 Lotfio Lakehal
+ * @license     MIT
+ * @link        https://github.com/lotfio-lakehal/timino
+ * 
+ * Copyright (C) 2018
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * INFO :
+ * DB connection class
+ * 
+ */
+namespace Timino\App\Services\Database;
+
+
+use Timino\App\Core\Linker;
+use Timino\App\Services\Template\ErrorTemplator;
+
+class Connection
+{
+   // single instance
+   private static $instance;
+   
+   //connection var
+   private $_con;
+   
+	private function __construct(){
+		try{
+			$this->_con = new \PDO(
+				Linker::database('DRIVER').":host=".Linker::database('HOST').";dbname=".Linker::database('NAME'),
+				Linker::database('USER'),
+				Linker::database('PASS'),
+				Linker::database('OPTIONS')
+			);
+		}catch(\PDOException $e)
+		{
+			die(ErrorTemplator::exceptionError($e->getMessage()));
+		}
+   }
+   
+	public static function instantiate()
+	{
+		if(!isset(self::$instance))
+		{
+			self::$instance = new self;
+		}
+		return self::$instance;
+   }
+   
+	/**
+	 * Get Connection
+	 * @return db connection Object
+	 */
+	public function con()
+	{
+		return $this->_con;
+	}
+}
