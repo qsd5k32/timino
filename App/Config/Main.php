498a3a4080bc39602f1262d472b07cb380367b2b
lotfio lakehal <contact@lotfio-lakehal.com>
2017-08-25 21:28:27 +0100

    main config file

---
+++ App/Config/Main.php
@@ -0,0 +1,43 @@
+<?php
+
+/** 
+ * Tinimo - PHP MVC framework
+ *
+ * @package     Tinimo
+ * @author      Lotfio Lakehal <contact@lotfio-lakehal.com>
+ * @copyright   2017 Lotfio Lakehal
+ * @license     MIT
+ * @link        https://github.com/lotfio-lakehal/timino
+ * 
+ * Copyright (C) 2018
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * INFO :
+ * Main Configuration file
+ * 
+ */
+
+defined("DS")     ||     define("DS", DIRECTORY_SEPARATOR);
+
+defined("ROOT")   ||     define('ROOT', dirname(dirname(__DIR__)) .DS);
+
+defined("APP")    ||     define('APP', ROOT . "App" .DS);
+
+defined("PUB")    ||     define('PUB', ROOT . "pub" .DS);
+
+defined("URL")    ||     define('URL',htmlspecialchars($_SERVER["REQUEST_SCHEME"], ENT_QUOTES, "UTF-8") . "://".htmlspecialchars($_SERVER["HTTP_HOST"], ENT_QUOTES, "UTF-8"). DS);
+
+defined("CONFIG") ||     define("CONFIG", APP . "Config".DS);
+
+defined("BNS")    ||     define('BNS', "Timino\\App\\");
+
+defined("SRV")    ||     define("SRV", BNS . "Services\\");
