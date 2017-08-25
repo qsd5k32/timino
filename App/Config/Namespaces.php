01e3737f516f859d639b4aaa679e7a95f5be022f
lotfio lakehal <contact@lotfio-lakehal.com>
2017-08-25 21:28:34 +0100

    namspaces config file

---
+++ App/Config/Namespaces.php
@@ -0,0 +1,34 @@
+<?php
+
+/** 
+ * Tinimo - PHP MVC framework
+ *
+ * @package     Tinimo
+ * @author      Lotfio Lakehal <contact@lotfio-lakehal.com>
+ * @copyright   2017 Lotfio Lakehal
+ * @license     MIT
+ * @link        https://github.com/lotfio-lakehal/timino
+ * 
+ * Copyright (C) 2018
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * INFO :
+ * namespaces Configuration file
+ * 
+ */
+
+return [
+
+   "CONTROLLERS" => BNS . "Controllers\\",
+   "MODELS"      => BNS . "Models\\",
+
+];
