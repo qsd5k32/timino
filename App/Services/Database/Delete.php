3ac02db7d627cab8615e192c3bbb6653eca9fd48
lotfio lakehal <contact@lotfio-lakehal.com>
2017-08-25 23:11:39 +0100

    active record

--- App/Services/Database/Delete.php
+++ App/Services/Database/Delete.php
@@ -30,5 +30,106 @@ namespace Timino\App\Services\Database;
 
 class Delete
 {
-
+   /**
+    * delete from db method
+    *
+    * @param string $tbl table name
+    * @param array $cond
+    * exemple of use cond = WHERE ....
+    * array("Name | = or != or < or > or <= exetra " => "lotfio") // one condition
+    * array(
+    *   "Name  | =  " => "ahmed",
+    *   "Email | != " => "admin@gmail.com" and so on
+    *) // multiple conditions
+    * @return void
+    */
+   public function from($tbl, $cond = NULL)
+   {
+   
+      $sql = "DELETE FROM `$tbl` WHERE ";
+   
+      if(is_array($cond) && count($cond) > 1 )
+      {
+         $keys   = array_keys($cond);
+         $values = array_values($cond);
+   
+   
+         foreach($keys as $key)
+         {
+            $newKeys[] = explode("|", $key);
+         }
+   
+         foreach($values as $val)
+         {
+            $newVals[] = explode("|", $val);
+         }
+   
+         // named placeholders
+         for($i = 0; $i < count($newKeys); $i++)
+         {
+            $nph[] =  trim($newKeys[$i][0]);
+         }
+   
+         // values 
+         for($i = 0; $i < count($newVals); $i++)
+         {
+            $insVals[] =  trim($newVals[$i][0]);
+         }
+   
+         $nph = array_map(function($elem){ return ":".$elem.rand();}, $nph);
+   
+   
+         for($i = 0; $i < count($keys) - 1; $i++)
+         {
+            $sql .= trim($newKeys[$i][0]). " " . trim($newKeys[$i][1]) ." " .  trim($nph[$i]) . " " . trim($newVals[$i][1]) . " ";
+         }
+   
+         $sql .= trim($newKeys[count($newKeys) - 1][0]). " " . trim($newKeys[count($newKeys) - 1][1]) ." " .  trim($nph[count($nph) - 1]);
+   
+   
+      }else{
+   
+         $keys   = array_keys($cond);
+         $values = array_values($cond);
+   
+         foreach($keys as $key)
+         {
+            $newKeys[] = explode("|", $key);
+         }
+   
+         foreach($values as $val)
+         {
+            $newVals[] = explode("|", $val);
+         }
+   
+         // named placeholders
+         for($i = 0; $i < count($newKeys); $i++)
+         {
+            $nph[] =  trim($newKeys[$i][0]);
+         }
+   
+         // values 
+         for($i = 0; $i < count($newVals); $i++)
+         {
+            $insVals[] =  trim($newVals[$i][0]);
+         }
+   
+   
+         $nph = array_map(function($elem){ return ":".$elem.rand();}, $nph);
+   
+   
+         $sql .= trim($newKeys[0][0]) ." " . trim($newKeys[0][1]) . " " .$nph[0];
+   
+   
+   
+      }
+   
+      //data to be bound;
+      $binds = array_combine($nph, $insVals);
+   
+   
+      $stmt = $this->db->prepare($sql);
+      $stmt->execute($binds);
+      return $stmt->rowCount() > 0 ? true : false;
+   }
 }
