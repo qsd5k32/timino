9e1817968518e38de5674b80bd50b4a9abe05e6d
lotfio lakehal <contact@lotfio-lakehal.com>
2017-08-27 23:32:05 +0100

    login frm test

--- App/Views/Index/Manage.php
+++ App/Views/Index/Manage.php
@@ -1,16 +1,24 @@
-<div class="container">
+<div class="container mg-top-30">
    <div class="row no-gutters">
-      <div class="col-md-6">
-         
-         <div class="panel panel-default">
-            <div class="panel-heading">
-               Panel default
-            </div>
-            <div class="panel-body">
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero unde pariatur nobis doloremque quasi odio accusamus quaerat ad quas omnis et eveniet quos labore quis minus voluptatum alias, nam voluptatem.
-            </div>
+      <div class="col-md-3"></div>
+         <div class="col-md-6">
+               <!-- form -->  
+                  <form action="" class="frm">
+                     <div class="inp-holder">
+                        <label for="name">User Name :</label>
+                        <input type="text" name="Name" class="input" id="name">
+                     </div>
+                     <div class="inp-holder">
+                        <label for="name">User Email :</label>
+                        <input type="text" name="Email" class="input" id="name">
+                     </div>
+                     <div class="inp-holder mg-top-20">
+                        <input type="submit" name="Email" class="btn btn-white btn-block" id="name">
+                     </div>
+                  
+                  </form> 
+               <!-- // end form -->
          </div>
-
       </div>
    </div>  
 </div>
