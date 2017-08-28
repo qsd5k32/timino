ec01d71cabf7ad22ee073cf6a2bec788fbe37d00
lotfio lakehal <contact@lotfio-lakehal.com>
2017-08-28 22:25:30 +0100

    test

--- App/Views/Index/Manage.php
+++ App/Views/Index/Manage.php
@@ -1,24 +1,16 @@
 <div class="container mg-top-30">
    <div class="row no-gutters">
-      <div class="col-md-3"></div>
-         <div class="col-md-6">
-               <!-- form -->  
-                  <form action="" class="frm">
-                     <div class="inp-holder">
-                        <label for="name">User Name :</label>
-                        <input type="text" name="Name" class="input" id="name">
-                     </div>
-                     <div class="inp-holder">
-                        <label for="name">User Email :</label>
-                        <input type="text" name="Email" class="input" id="name">
-                     </div>
-                     <div class="inp-holder mg-top-20">
-                        <input type="submit" name="Email" class="btn btn-white btn-block" id="name">
-                     </div>
-                  
-                  </form> 
-               <!-- // end form -->
-         </div>
+      <div class="col-md-6">
+            <pre>
+                  <code class="language-php">
+                        echo $this->record->insert->into("users", [
+                              "Email"   => $email,
+                              "Name "   => $name,
+                              "Passwd " => $passwd
+                        ]);
+                  </code>
+            </pre>
+      </div>
       </div>
    </div>  
 </div>
