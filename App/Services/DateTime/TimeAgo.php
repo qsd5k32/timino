6fee229f32a2b21a7a7187e94ef58f4348a04bad
lotfio lakehal <contact@lotfio-lakehal.com>
2017-08-28 13:44:24 +0100

    arabic elapced time

--- App/Services/DateTime/TimeAgo.php
+++ App/Services/DateTime/TimeAgo.php
@@ -37,7 +37,7 @@ class TimeAgo
     * @param boolean $full show full elapced time years months hours minutes seconds
     * @return string
     */
-   public function timeAgo($datetime, $full = false) {
+   public function english($datetime, $full = false) {
 
       $now = new \DateTime;
       $ago = new \DateTime($datetime);
@@ -65,6 +65,44 @@ class TimeAgo
   
       if (!$full) $string = array_slice($string, 0, 1);
       return $string ? implode(', ', $string) . ' ago' : 'just now';
+  }
+
+   /**
+    * elapced time methos
+    *
+    * @param string $datetime TIMESTAMP string
+    * @param boolean $full show full elapced time years months hours minutes seconds
+    * @return string
+    */
+   public function arabic($datetime, $full = false) {
+
+      $now = new \DateTime;
+      $ago = new \DateTime($datetime);
+      $diff = $now->diff($ago);
+  
+      $diff->w = floor($diff->d / 7);
+      $diff->d -= $diff->w * 7;
+  
+      $string = array(
+          'y' => 'سنة',
+          'm' => 'شهر',
+          'w' => 'اسبوع',
+          'd' => 'يوم',
+          'h' => 'ساعة',
+          'i' => 'دقيقة',
+          's' => 'ثانية',
+      );
+      
+      foreach ($string as $k => &$v) {
+          if ($diff->$k) {
+              $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? ' ' : '');
+          } else {
+              unset($string[$k]);
+          }
+      }
+  
+      if (!$full) $string = array_slice($string, 0, 1);
+      return $string ? ' منذ ' . implode(', ', $string) : ' الان ';
   }
 
 }
