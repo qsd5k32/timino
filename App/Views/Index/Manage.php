95d21e0ed484094ef865cf24eb5775aefb84dbf6
lotfio lakehal <contact@lotfio-lakehal.com>
2017-09-02 22:37:00 +0100

    index page html css mod

--- App/Views/Index/Manage.php
+++ App/Views/Index/Manage.php
@@ -1,21 +1,23 @@
 <div class="container mg-top-30 index">
       <div class="row no-gutters">
-            <div class="col-md-12 center-txt">
+            <div class="col-md-3"></div>
+
+            <div class="col-md-6 center-txt">
+
                   <h1 class="center-txt">Timino PHP MVC v1.0 beta</h1>
                   <img src="<?=$uploads->IMG . 'logo.png'?>" alt="logo" title="Timino" class="logo">
                   <div class="mg-top-50"></div>
-                  <hr style="border-color:#eee; width:60%; margin:auto">
-                  <ul class="nav">
-                        <div class="mg-top-10"></div>
-                        <li><a href="#">News</a></li>
-                        <li><a href="#">Documentation</a></li>
-                        <li><a href="#">Release</a></li>
-                        <li><a href="#">Fork</a></li>
-                        <li><a href="#">Donate</a></li>
-                  </ul>
+      
             </div>
-            <div class="footer center-txt">
-                  Timino 2018 &copy;
-            </div>
+
+            <div class="col-md-3"></div>
+
+            <div class="col-md-12">
+                  <div class="footer center-txt">
+                        Timino 2018 &copy;
+                  </div>
+            </div>      
+
+
       </div>  
 </div>
