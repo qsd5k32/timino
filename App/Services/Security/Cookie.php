f485d006ef3f8368013c8c68f2b11ef5993f0ec8
lotfio lakehal <contact@lotfio-lakehal.com>
2017-09-03 14:09:37 +0100

    cookie service class

--- App/Services/Security/Cookie.php
+++ App/Services/Security/Cookie.php
@@ -30,4 +30,53 @@ namespace Timino\App\Services\Security;
 
 class Cookie
 {
+    /**
+     * @param string $name cookie name
+     * @param string $value cookie value
+     * @param $exp
+     */
+    public function set($name, $value, $exp)
+    {
+       setcookie($name, $value, $exp, "/");
+    }
+
+    /**
+     * @param string $name cookie name
+     * @return mixed
+     */
+    public function get($name)
+    {
+        return isset($_COOKIE[$name]) ? $_COOKIE[$name] : false;
+    }
+
+    /**
+     * @param $name cookie name
+     * @param $value cookie value
+     * @param $exp
+     */
+    public function serialize($name, $value, $exp)
+    {
+        setcookie($name, \serialize($value), $exp, "/");
+    }
+
+    /**
+     * @param $name cookie name
+     * @return bool|mixed
+     */
+    public function unserialize($name)
+    {
+        if(isset($_COOKIE[$name]))
+        {
+            return \unserialize($_COOKIE[$name]);
+        }
+        return false;
+    }
+    /**
+     * @param $name cookie name
+     */
+    public function remove($name)
+    {
+        if(isset($_COOKIE[$name])) $this->set($name, NULL , time() - 100000);
+    }
+
 }
