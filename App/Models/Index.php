5c59bd13ba5f1e0ffc6cd4727e601e4c3bbbac84
lotfio lakehal <contact@lotfio-lakehal.com>
2017-09-06 18:21:34 +0100

    remove examples

--- App/Models/Index.php
+++ App/Models/Index.php
@@ -41,23 +41,22 @@ class Index extends Model
       //    "Passwd" => sha1("123456")
       // ));
 
-
       // $this->record->update->set("users", array(
-      //    "Email" => "bistish@000000000000"
+      //    "Email" => "bilal@000000000000"
       // ), array(
-      //    "Email | = " => "lotfio@ouedkniss.com | and",
-      //    "Name  | = " => "Lakehal"
+      //    "Email | = " => "bilal@gmail.com | and",
+      //    "Name  | = " => "timino"
       // ));
       
       // delete
       // echo $this->record->delete->from("users",array(
       //    "Email | = " => "lotfio@gmail.com | and",
-      //    "Name  | = " => "saida" 
+      //    "Name  | = " => "timino"
       // ));
 
       // select
       //  $do = $this->record->select->from("users", array("Email"), array(
-      //     "Name | = " => "lotfi"
+      //     "Name | = " => "timino"
 
       //  ), "ORDER BY ID ");
 
