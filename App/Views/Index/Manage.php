536f5ce6c31fdf99adf164bb6507fc19c8fee843
lotfio lakehal <lotfiolakehal@gmail.com>
2017-11-19 15:15:48 +0100

    update default template

--- App/Views/Index/Manage.php
+++ App/Views/Index/Manage.php
@@ -2,19 +2,20 @@
     <div class="columns mt-1">
         <div class="column">
             <div class="col-12">
-                <span class="label label-default custom-label hide-sm">Current branch : <span class="text-error"><?=$modelData->branch?></span></span>
+                <span class="label float-left hide-sm"><b>Current branch</b> : <span><?=$modelData->branch?></span></span>
+                <span class="label float-right"><b>Version </b>: 1.0.0</span></span>
                 <!--#link#-->
+                <div class="clear-fix"></div>
             </div>
             <div class="mb-2"></div>
         </div>
     </div>
 
-    <div class="columns">
-
+    <div class="columns mt-2">
         <div class="column col-6 col-sm-8 col-mx-auto text-center">
-            <h1>Timino PHP MVC 1.0.0</h1>
+            <h1>Timino PHP MVC</h1>
             <p>Default View no templating engine.</p>
-            <img src="<?=$uploads->IMG . 'logo.png'?>" alt="logo" title="Timino" class="logo">
+            <img src="<?=$uploads->IMG . 'logo.png'?>" alt="logo" title="Timino" class="logo mt-2">
         </div>
 
         <div class="column col-12 mt-4 text-center links">
