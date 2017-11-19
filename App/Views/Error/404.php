44a9a3239ff98a2284c72e320179ddd4bdae6810
lotfio lakehal <lotfiolakehal@gmail.com>
2017-11-19 15:48:34 +0100

    update error view

--- App/Views/Error/404.php
+++ App/Views/Error/404.php
@@ -1,20 +1,25 @@
 <div class="container grid-xl">
 
     <div class="columns">
-        <div class="column col-6 col-sm-8 col-mx-auto text-center mt-2 col-mr-auto">
-<!--            <img src="=$uploads->IMG . '404.png'<!--" class="res-img" alt="404">-->
+        <div class="column col-8 col-mx-auto text-center mt-2 col-mr-auto">
             <img src="<?=$uploads->IMG . '404.png'?>" class="res-img" alt="404">
         </div>
+
+        <div class="column col-8  col-mx-auto text-center mt-2 col-mr-auto">
+            <h1>Page not found</h1>
+        </div>
+
     </div>
 
     <div class="columns">
-        <div class="column col-6 col-sm-8 col-xs-10 col-mx-auto text-center">
+        <div class="column col-8 col-xs-10 col-mx-auto text-center">
             <div>
                 <p>Oops we can't seem to find the page you are looking for. the page could be deleted or maybe you followed a wrong link ! please go back to the home page and enjoy your stay on the website . if you have figured out any problems browsing the website or any security issues please let us know via the email below. Thanks !</p>
                 <div class="mt-2"></div>
                 <a href="/" class="btn btn-primary bg-warning">back to website</a>
                 <div class="mt-2"></div>
                 <a href="#">lotfiolakehal@gmail.com</a>
+                <div class="mt-2"></div>
             </div>
         </div>
     </div>
